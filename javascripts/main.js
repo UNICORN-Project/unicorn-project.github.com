8f70d08ee9548e9f08488090a080e1ef5f526c15
saimushi <saimushi@10.1.108.77>
2015-07-21 16:44:40 +0900

    ReadmeマークダウンをgithubページのTOPでも表示するように修正

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
 console.log('This would be the main JS file.');
+$(document).ready(function(){
+    var target = $("#markdown_content");
+
+    $.ajax({
+        url: target[0].attributes["src"].value,
+    }).success(function(data){
+        target.append(marked(data));
+    }).error(function(data){
+        target.append("This content failed to load.");
+    });
+});
