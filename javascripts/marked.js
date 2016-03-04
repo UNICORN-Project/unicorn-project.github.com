8e9f1ba587852d67eb02614d55bf4e8e35a46a40
saimushi <saimushi@gmail.com>
2016-03-04 13:02:39 +0900

    Update marked.js

--- javascripts/marked.js
+++ javascripts/marked.js
@@ -897,7 +897,7 @@ Renderer.prototype.image = function(href, title, text) {
 };
 
 Renderer.prototype.text = function(text) {
-  return text.replace(/\n/g, '<br/>');
+  return text.replace(/\n/g, '</p><p>');
 };
 
 /**
