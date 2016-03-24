c0ab9f405ce8132b9fc529d701f22de0519a970d
UNICORN-Project <saimushi+unicorn-project@gmail.com>
2016-03-24 11:35:35 +0900

    Update landing-page.js

--- assets/vendor/js/bootstrap/landing-page.js
+++ assets/vendor/js/bootstrap/landing-page.js
@@ -7,7 +7,7 @@ $(function() {
         }
         if (0 < $($anchor).size()){
         	$('html, body').stop().animate({
-        		scrollTop: $($anchor).offset().top
+        		scrollTop: $($anchor).offset().top -20
         	}, 1000, 'easeInOutExpo');
         	event.preventDefault();
         }
