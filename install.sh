85798b722ca1ee7372e0cadaa10a1146e8c6eb81
saimushi <saimushi@192.168.1.142>
2016-10-28 10:58:18 +0900

    mvに-rは無いんだった・・・

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 curl -sSL https://github.com/UNICORN-Project/UNICORN/archive/0.2.0.zip > ./UNICORN.zip && \
 unzip -o ./UNICORN.zip
 rm ./UNICORN.zip
-mv -rf ./UNICORN-0.2.0/* ./
+mv -f ./UNICORN-0.2.0/* ./
 rm -rf ./UNICORN-0.2.0
 curl -sSL https://github.com/UNICORN-Project/lib/archive/0.2.0.zip > ./lib.zip && \
 unzip -o ./lib.zip -d lib
 rm ./lib.zip
-mv -rf ./lib-0.2.0/* ./lib
+mv -f ./lib-0.2.0/* ./lib
 rm -rf ./lib-0.2.0
