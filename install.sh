fb014c58b9ee993efecb7ab399890de07ff0f059
saimushi <saimushi@192.168.1.142>
2016-10-28 10:59:32 +0900

    libのmvはディレクトリ名の変更で

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ rm -rf ./UNICORN-0.2.0
 curl -sSL https://github.com/UNICORN-Project/lib/archive/0.2.0.zip > ./lib.zip && \
 unzip -o ./lib.zip -d lib
 rm ./lib.zip
-mv -f ./lib-0.2.0/* ./lib
+mv -f ./lib-0.2.0 ./lib
 rm -rf ./lib-0.2.0
