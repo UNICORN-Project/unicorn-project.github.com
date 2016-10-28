edac7b645190a80194363464a2152cc5587abad2
saimushi <saimushi@192.168.1.142>
2016-10-28 11:44:50 +0900

    インストーラ調整中

--- install.sh
+++ install.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 # configのバックアップ
+mkdir -p ./__tmp
 if [ -e ./lib/FrameworkPackage/core/config.xml ]; then
-  cp -f ./lib/FrameworkPackage/core/config.xml ./_tmp/config.xml.bak
+  cp -f ./lib/FrameworkPackage/core/config.xml ./__tmp/config.xml.bak
 fi
 if [ -e ./lib/FrameworkPackage/core/config.xml ]; then
-  cp -f ./lib/FrameworkPackage/core/package.xml ./_tmp/package.xml.bak
+  cp -f ./lib/FrameworkPackage/core/package.xml ./__tmp/package.xml.bak
 fi
 if [ -e ./lib/FrameworkPackage/core/FrameworkManager.config.xml ]; then
-  cp -f ./lib/FrameworkManager/core/FrameworkManager.config.xml ./_tmp/FrameworkManager.config.xml.bak
+  cp -f ./lib/FrameworkManager/core/FrameworkManager.config.xml ./__tmp/FrameworkManager.config.xml.bak
 fi
 if [ -e ./lib/FrameworkPackage/core/FrameworkManager.package.xml ]; then
-  cp -f ./lib/FrameworkManager/core/FrameworkManager.package.xml ./_tmp/FrameworkManager.package.xml.bak
+  cp -f ./lib/FrameworkManager/core/FrameworkManager.package.xml ./__tmp/FrameworkManager.package.xml.bak
 fi
 if [ -e ./README.md ]; then
-  cp -f ./README.md ./_tmp/README.md
+  cp -f ./README.md ./__tmp/README.md
 fi
 
 curl -sSL https://github.com/UNICORN-Project/UNICORN/archive/0.2.0.zip > ./UNICORN.zip && \
@@ -25,7 +26,7 @@ rm -rf ./UNICORN-0.2.0
 curl -sSL https://github.com/UNICORN-Project/lib/archive/0.2.0.zip > ./lib.zip && \
 unzip -o ./lib.zip
 rm ./lib.zip
-mv -f ./lib-0.2.0 ./lib
+mv -Rf ./lib-0.2.0 ./lib
 rm -rf ./lib-0.2.0
 rm -rf ./lib/.gitignore
 mkdir -p ./vendor/UNICORN
@@ -35,21 +36,22 @@ mv -f ./composer.json ./vendor/UNICORN/
 mv -f ./composer.phar ./vendor/UNICORN/
 
 # configバックアップの初期化
-if [ ! -e ./_tmp/config.xml.bak ]; then
-  cp -f ./lib/FrameworkPackage/core/config.xml ./_tmp/config.xml.bak
+if [ ! -e ./__tmp/config.xml.bak ]; then
+  cp -f ./lib/FrameworkPackage/core/config.xml ./__tmp/config.xml.bak
 fi
-if [ ! -e ./_tmp/package.xml.bak ]; then
-  cp -f ./lib/FrameworkPackage/core/package.xml ./_tmp/package.xml.bak
+if [ ! -e ./__tmp/package.xml.bak ]; then
+  cp -f ./lib/FrameworkPackage/core/package.xml ./__tmp/package.xml.bak
 fi
-if [ ! -e ./_tmp/FrameworkManager.config.xml.bak ]; then
-  cp -f ./lib/FrameworkManager/core/FrameworkManager.config.xml ./_tmp/FrameworkManager.config.xml.bak
+if [ ! -e ./__tmp/FrameworkManager.config.xml.bak ]; then
+  cp -f ./lib/FrameworkManager/core/FrameworkManager.config.xml ./__tmp/FrameworkManager.config.xml.bak
 fi
-if [ ! -e ./_tmp/FrameworkManager.package.xml.bak ]; then
-  cp -f ./lib/FrameworkManager/core/FrameworkManager.package.xml ./_tmp/FrameworkManager.package.xml.bak
+if [ ! -e ./__tmp/FrameworkManager.package.xml.bak ]; then
+  cp -f ./lib/FrameworkManager/core/FrameworkManager.package.xml ./__tmp/FrameworkManager.package.xml.bak
 fi
-if [ -e ./_tmp/README.md ]; then
-  mv -f ./_tmp/README.md ./_tmp/README.md
+if [ -e ./__tmp/README.md ]; then
+  mv -f ./__tmp/README.md ./__tmp/README.md
 fi
+mv -Rf ./__tmp ./_tmp
 
 # UNICORNのインストーラを実行
 php ./lib/UNICORN
