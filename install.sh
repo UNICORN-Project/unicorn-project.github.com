83963b35f913d0b1889a3aca0d309798109de81b
saimushi <saimushi@192.168.1.142>
2016-10-28 10:54:25 +0900

    インストーラ作成中

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 curl -sSL https://github.com/UNICORN-Project/UNICORN/archive/0.2.0.zip > ./UNICORN.zip && \
-unzip UNICORN.zip
-rm UNICORN.zip
+unzip -o ./UNICORN.zip
+rm ./UNICORN.zip
+mv -rf ./UNICORN-0.2.0/* ./
+rm -rf ./UNICORN-0.2.0
 curl -sSL https://github.com/UNICORN-Project/lib/archive/0.2.0.zip > ./lib.zip && \
-unzip lib.zip
-rm lib.zip
+unzip -o ./lib.zip -d lib
+rm ./lib.zip
+mv -rf ./lib-0.2.0/* ./lib
+rm -rf ./lib-0.2.0
