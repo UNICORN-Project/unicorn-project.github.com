38194fcb0ddc5ce7cc63b468f423da8414b690b1
saimushi <saimushi@192.168.1.142>
2016-10-28 10:32:51 +0900

    リダイレクトしてcurlする

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sS https://github.com/UNICORN-Project/UNICORN/archive/0.2.0.zip > ./UNICORN.zip && \
-unzip UNICORN.zip
+curl -sSL https://github.com/UNICORN-Project/UNICORN/archive/0.2.0.zip > ./UNICORN.zip && \
+unzip -j UNICORN.zip
 rm UNICORN.zip
-curl -sS https://github.com/UNICORN-Project/lib/archive/0.2.0.zip > ./lib.zip && \
+curl -sSL https://github.com/UNICORN-Project/lib/archive/0.2.0.zip > ./lib.zip && \
 unzip lib.zip
 rm lib.zip
